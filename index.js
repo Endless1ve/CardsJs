52b50274f62936686e668fbf966afac376697d24
Artem Tulupov <stalin.dignitas70@gmail.com>
2023-10-10 14:19:32 +0300

    add initial cards render

--- index.js
+++ index.js
@@ -2,84 +2,104 @@
 const popup = document.querySelector('.popup');
 const openPopupButton = document.querySelector('.profileButton');
 const closePopupButton = document.querySelector('.popupClose');
-
+//form variables
+const form = document.forms.newCard;
 //cardsVariables
 const cardsContainer = document.querySelector('.cardsBlock');
 const likeButton = document.querySelectorAll('.cardLike');
 const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    },
-    {
-      name: 'Нургуш',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg'
-    },
-    {
-      name: 'Тулиновка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg'
-    },
-    {
-      name: 'Остров Желтухина',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg'
-    },
-    {
-      name: 'Владивосток',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg'
-     }
-  ];
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  },
+  {
+    name: 'Нургуш',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg'
+  },
+  {
+    name: 'Тулиновка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg'
+  },
+  {
+    name: 'Остров Желтухина',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg'
+  },
+  {
+    name: 'Владивосток',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg'
+  }
+];
 
 
 //cards functions
-function cardsInteractions () {
-    function likeCard(event) {
-        if(event.target.classList.contains('cardLike')) {
-            event.target.classList.toggle('cardLike-active');
-        }
-    }
-    function deleteCard(event) {
-        cardsContainer.removeChild(event.target.closest('.card'))
-    }
-    return {likeCard, deleteCard};
+function addCard(event) {
+  event.preventDefault();
 }
 
-const cardInteraction = cardsInteractions();
+function renderInitialCards(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const card =
+      `<div class="card">
+      <div class="cardImage" style='background-image: url(${arr[i].link})'>
+        <button class="cardDeleteButton"></button>
+      </div>
+      <div class="cardDescription">
+        <h3 class="cardName">${arr[i].name}</h3>
+        <button class="cardLike"></button>
+      </div>
+    </div>`;
+    cardsContainer.insertAdjacentHTML('beforeend', card)
+  }
+}
+function likeCard(event) {
+  if (event.target.classList.contains('cardLike')) {
+    event.target.classList.toggle('cardLike-active');
+  }
+}
+function deleteCard(event) {
+  if (event.target.classList.contains('cardDeleteButton')) {
+    cardsContainer.removeChild(event.target.closest('.card'))
+  }
+}
 
+renderInitialCards(initialCards);
 
-//card event
-cardsContainer.addEventListener('click', cardInteraction.likeCard);
-cardsContainer.addEventListener('click', cardInteraction.deleteCard);
 
 //popup functions
 function openPopup() {
-    popup.classList.add('popup-active');
-    closePopupButton.addEventListener('click', closePopup);
+  popup.classList.add('popup-active');
+  closePopupButton.addEventListener('click', closePopup);
 }
 
 function closePopup() {
-    popup.classList.remove('popup-active');
-    closePopupButton.removeEventListener('click', closePopup);
+  popup.classList.remove('popup-active');
+  closePopupButton.removeEventListener('click', closePopup);
 }
+
+//card event
+cardsContainer.addEventListener('click', likeCard);
+cardsContainer.addEventListener('click', deleteCard);
+
 
 //popup event 
 openPopupButton.addEventListener('click', openPopup);

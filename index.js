721c68e54e3d804442f8524d3cc8c9ac343311e8
Artem Tulupov <stalin.dignitas70@gmail.com>
2023-10-11 18:29:27 +0300

    add like card and saves state in local storage

--- index.js
+++ index.js
@@ -2,61 +2,61 @@ const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    likes: 0,
+    isLiked: false,
     id: '1'
   },
   {
     name: 'Челябинская область',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    likes: 0,
+    isLiked: false,
     id: '2'
   },
   {
     name: 'Иваново',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    likes: 0,
+    isLiked: false,
     id: '3'
   },
   {
     name: 'Камчатка',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    likes: 0,
+    isLiked: false,
     id: '4'
   },
   {
     name: 'Холмогорский район',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    likes: 0,
+    isLiked: false,
     id: '5'
   },
   {
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    likes: 0,
+    isLiked: false,
     id: '6'
   },
   {
     name: 'Нургуш',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg',
-    likes: 0,
+    isLiked: false,
     id: '7'
   },
   {
     name: 'Тулиновка',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg',
-    likes: 0,
+    isLiked: false,
     id: '8'
   },
   {
     name: 'Остров Желтухина',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg',
-    likes: 0,
+    isLiked: false,
     id: '9'
   },
   {
     name: 'Владивосток',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg',
-    likes: 0,
+    isLiked: false,
     id: '10'
   }
 ];
@@ -90,7 +90,7 @@ function formValidation() {
     submitButton.classList.add('popupButton-active');
   }
 }
-function addCard(name, link, id) {
+function addCard(name, link, id, isLiked) {
   const card = `
   <div class="card" id="${id}">
     <div class="cardImage" style="background-image: url('${link}')">
@@ -109,7 +109,6 @@ function renderCard(event) {
   const nameInput = addCardForm.elements.name;
   const linkInput = addCardForm.elements.link;
   const id = Math.random().toString(16).slice(2);
-
   const newCard = addCard(nameInput.value, linkInput.value, id);
   addNewLocalCard(nameInput.value, linkInput.value, id);
 
@@ -125,7 +124,8 @@ function addLocalCards(array) {
 }
 function addNewLocalCard(name, link, id) {
   const localArr = Array.from(JSON.parse(localStorage.cards));
-  const newCard = { name, link, id };
+  const isLiked = false;
+  const newCard = { name, link, id, isLiked };
   localArr.push(newCard);
   localStorage.setItem('cards', JSON.stringify(localArr));
 }
@@ -133,8 +133,12 @@ function renderLocalCards() {
   addLocalCards(initialCards);
   const cards = Array.from(JSON.parse(localStorage.cards));
   for (let i = 0; i < cards.length; i++) {
-    const newCard = addCard(cards[i].name, cards[i].link, cards[i].id);
+    const newCard = addCard(cards[i].name, cards[i].link, cards[i].id, cards[i].isLiked);
     cardsBlock.insertAdjacentHTML('beforeend', newCard);
+    if (cards[i].isLiked) {
+      const card = document.getElementById(cards[i].id);
+      const like = card.querySelector('.cardLike').classList.add('cardLike-active');
+    }
   }
 }
 function deleteCard(event) {
@@ -144,11 +148,25 @@ function deleteCard(event) {
     const filtered = cards.filter(item => item.id !== card.id);
     localStorage.setItem('cards', JSON.stringify(filtered));
     cardsBlock.removeChild(card);
+  }
+}
 
+function likeCard(event) {
+  const cards = Array.from(JSON.parse(localStorage.cards));
+  if (event.target.classList.contains('cardLike')) {
+    const card = event.target.closest('.card');
+    event.target.classList.toggle('cardLike-active');
+    cards.map(item => {
+      if (item.id === card.id) {
+        item.isLiked = !item.isLiked;
+      }
+    });
+    localStorage.setItem('cards', JSON.stringify(cards));
   }
 }
 
 renderLocalCards();
+cardsBlock.addEventListener('click', likeCard);
 cardsBlock.addEventListener('click', deleteCard);
 addCardForm.addEventListener('submit', renderCard);
 openPopupButton.addEventListener('click', openPopup);
